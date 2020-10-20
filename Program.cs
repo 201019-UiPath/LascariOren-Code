25df3625bc5c551b94284f7dc687079213f9a524
Aivansky <51238426+Aivansky@users.noreply.github.com>
2020-10-20 06:44:19 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//1 predefined namespace
+using System;
+//1 custom defined namespace
+namespace helloworld
+{
+    class Program
+    {
+        //execution starts here
+            //commandline arguments
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worldreeeeeeeeeee!");
+        }
+    }
+}
